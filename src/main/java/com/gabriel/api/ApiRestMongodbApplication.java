7c48456a752cc3661677435f8b835f9eaac7300f
Gabriel <52108752+GabrielTheRock@users.noreply.github.com>
2020-10-26 14:54:38 -0300

    Add files via upload

---
+++ src/main/java/com/gabriel/api/ApiRestMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestMongodbApplication.class, args);
+	}
+
+}
