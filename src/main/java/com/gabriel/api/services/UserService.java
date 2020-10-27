8f20a497ad5d3133c352fefc672618f3ea183bf0
gabrieltherock <gabrieldarocha2013@hotmail.com>
2020-10-27 12:53:01 -0300

    Implementing findById method and exceptions

--- src/main/java/com/gabriel/api/services/UserService.java
+++ src/main/java/com/gabriel/api/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabriel.api.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.api.domain.User;
 import com.gabriel.api.repository.UserRepository;
+import com.gabriel.api.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,14 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	/*
+	 * Optional serve para evitar que o objeto venha como null para a variável obj
+	 */
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
