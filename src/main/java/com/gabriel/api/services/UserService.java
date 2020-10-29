4fead22ca4b9d98472d462168cfffdd94468cc03
gabrieltherock <gabrieldarocha2013@hotmail.com>
2020-10-29 14:43:54 -0300

    Adding delete method and tratting exceptions

--- src/main/java/com/gabriel/api/services/UserService.java
+++ src/main/java/com/gabriel/api/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.api.domain.User;
+import com.gabriel.api.dto.UserDTO;
 import com.gabriel.api.repository.UserRepository;
 import com.gabriel.api.services.exception.ObjectNotFoundException;
 
@@ -27,5 +28,18 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 }
